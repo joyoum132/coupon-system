1592ce85f0b6a77a906d68ee79ad37e322d26cef
joyoum132 <yoojy01@gmail.com>
2024-03-30 12:05:23 +0900

    문제 시나리오 정의

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,5 +27,28 @@ class ApplyServiceTest {
         applyService.apply((1L));
         long count = couponRepository.count();
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    //문제 원인 : Race Condition
+    void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for(int i=0; i<threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
