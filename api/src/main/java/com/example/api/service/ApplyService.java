12d17d8702ed6060005601ee1194303724316c41
joyoum132 <yoojy01@gmail.com>
2024-03-30 12:21:48 +0900

    redis로 동시성 이슈 수정

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,21 +1,25 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplyService {
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     //쿠폰 발급
     public void apply(Long userId) {
         //쿠폰 개수 가져옴
-        long count = couponRepository.count();
+//        long count = couponRepository.count();
+        long count = couponCountRepository.increment();
 
         if(count > 100) return;
 
